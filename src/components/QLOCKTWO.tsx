88459c302bf265d03ed04061b773b43b6d20ab03
Braith Anderson <braithanderson1@gmail.com>
2022-04-08 19:08:35 +1000

    Added Seconds Button Not quite fully implemented

--- src/components/QLOCKTWO.tsx
+++ src/components/QLOCKTWO.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./QLOCKTWO.css";
 import { QLOCKTiles } from "./QLOCKTiles";
 import { QLOCKCornerIndicators } from "./QLOCKCornerIndicators";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ToggleButton from "@mui/material/Button";
 
 const QLOCKString =
   "itlisasampmacquarterdctwentyfivexhalfstenftopasterunineonesixthreefourfivetwoeightelevenseventwelvetenseoclock";
@@ -15,6 +17,7 @@ export type Time = {
 
 export function QLOCKTWO() {
   const [date, setDate] = useState(new Date());
+  const [secondsSelected, setSecondsSelected] = useState(false);
 
   useEffect(() => {
     const dateUpdateInterval = setInterval(() => {
@@ -30,11 +33,21 @@ export function QLOCKTWO() {
     setDate(new Date());
   }
   return (
-    <div>
+    <div className={"CentreWrapper"}>
       <div className="QLOCKTWOContainer">
         <QLOCKCornerIndicators additive={date.getMinutes() % 5} />
         <QLOCKTiles characterList={QLOCKTileChracters} fullDate={date} />
       </div>
+      <ToggleButton
+        className={"SecondsButton"}
+        variant={"contained"}
+        color={"primary"}
+        onChange={() => {
+          setSecondsSelected(!secondsSelected);
+        }}
+      >
+        <AccessTimeIcon />
+      </ToggleButton>
     </div>
   );
 }
